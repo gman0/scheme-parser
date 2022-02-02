043ef479b3335744133830e51984b673c87e6795
Robert Vasek <robert.vasek@cern.ch>
2022-02-02 22:56:18 +0100

    Main: added support for simple REPL
    Evaluates Scheme expression from args or runs an interactive shell.
    
        $ ./scheme-parser
        Type ":quit" or ":q" to exit the shell.
        scm>>> (+ 1 2)
        3
        scm>>>
    
        $ ./scheme-parser -- "(+ 1 2)"
        3

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,49 @@
 module Main where
 
-import Scheme.Value
 import Scheme.Primitives
-
+import Scheme.Value
 import System.Environment
+import System.IO
+
+printUsage :: IO String -> IO ()
+printUsage progNameM = do
+  progName <- progNameM
+  putStrLn "Scheme R5RS interpreter\n"
+  putStrLn $ "Usage: " ++ progName ++ " [EXPR]\n"
+  putStrLn " Positional arguments:\n"
+  putStrLn "\t[EXPR]\tOptional. Evaluate Scheme expression.\n"
+  putStrLn " If no EXPR is given, interactive Scheme shell will open."
+
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+evalString :: String -> IO String
+evalString expr =
+  let evaled = fmap show $ readExpr expr >>= eval
+   in return $ extractValue $ trapError evaled
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr = evalString expr >>= putStrLn
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred prompt action = do
+  result <- prompt
+  if pred result
+    then return ()
+    else action result >> until_ pred prompt action
+
+runRepl :: IO ()
+runRepl = do
+  putStrLn "Type \":quit\" or \":q\" to exit the shell."
+  until_ (`elem` [":quit", ":q"]) (readPrompt "scm>>> ") evalAndPrint
 
 main :: IO ()
 main = do
   args <- getArgs
-  let evaled = fmap show $ readExpr (head args) >>= eval
-  putStrLn $ extractValue $ trapError evaled
+  case length args of
+    0 -> runRepl
+    1 -> evalAndPrint $ head args
+    _ -> putStrLn "Error: Program takes only zero or one argument." >> printUsage getProgName
